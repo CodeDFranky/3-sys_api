adddc74166c81e4d3fe938cf4f749ce78deeb7f4
Franco Ramos <d.franco.ramos1@gmail.com>
2022-10-22 11:24:13 +0800

    basic dm generation working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const bitgener = require('bitgener');
+const bwipjs = require('bwip-js');
 
 const port = process.env.PORT || 3000;
-
 const api = express();
 
 api.use(express.static(__dirname + '/public'));
@@ -15,5 +16,34 @@ api.listen(port, () => {
 })
 
 api.post('/generate', (req, res) => {
-    console.log('Post request received!');
+    const reqQueryObj = req.query;
+    const message = reqQueryObj.message ? reqQueryObj.message : '';
+    const secret = reqQueryObj.secret ? reqQueryObj.secret : '';
+    const title = reqQueryObj.altText ? reqQueryObj.altText : '';
+
+
+    let options = {
+        bcid: 'datamatrix', // Barcode type
+        text: message, // Text to encode
+        alttext: title, // Additional text
+        scale: 5, // scaling factor
+        includetext: title.length > 0 ? true : false, // Show human-readable text
+        textxalign: 'center', // Always good to set this
+        textsize: 3,
+        padding: 2,
+        backgroundcolor: 'ffffff',
+        // dotty: true
+    }
+
+    bwipjs.toBuffer(options, function (err, png) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Content-Length': png.length
+            });
+            res.end(png);
+        }
+    });
 })
